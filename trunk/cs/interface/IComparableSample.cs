338eb585b59abd111beddc20acc5bc17bf560de1
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-02 15:37:17 +0000

    IComparable のサンプル追加

---
+++ trunk/cs/interface/IComparableSample.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+///   IComparable のサンプル
+/// </summary>
+///
+/// コンパイル : csc IComparableSample.cs Point.cs
+
+using System;
+
+
+namespace IComparableSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    // Point を格納したデータを準備
+	    Point [] ary = {
+		new Point(1, 2),
+		new Point(3, 5),
+		new Point(1, 9),
+		new Point(2, 8),
+		new Point(4, 1),
+		new Point(3, 1)
+	    };	    
+
+	    Console.WriteLine("==== Original ====");
+	    foreach (Point pos in ary)
+	    {
+		Console.Write("{0} ", pos);
+	    }
+	    Console.WriteLine();
+
+	    // データのソート
+	    Array.Sort(ary);
+
+	    // ソート結果を出力
+	    Console.WriteLine("==== Sorted ====");
+	    foreach (Point pos in ary)
+	    {
+		Console.Write("{0} ", pos);
+	    }
+	    Console.WriteLine();	    
+        }
+    }
+}
---
+++ trunk/cs/interface/Point.cs
@@ -0,0 +1,57 @@
+﻿/// <summary>
+///   点クラス (IComparable のサンプル用)
+/// </summary>
+
+using System;
+
+namespace IComparableSample
+{
+    class Point : IComparable
+    {
+	/// <summary>
+	///   x 座標
+	/// </summary>
+	public int x  { get; set; }
+
+	/// <summary>
+	///   y 座標
+	/// </summary>
+	public int y  { get; set; }
+
+
+	/// <summary>
+	/// 座標値 (x, y) を与えて初期化。
+	/// </summary>
+	/// <param name="x">x 座標値</param>
+	/// <param name="y">y 座標値</param>
+	public Point(int x, int y)
+	{
+	    this.x = x;
+	    this.y = y;
+	}
+
+
+	/// <summary>
+	///   比較メソッド
+	/// </summary>
+	public int CompareTo(Object obj)
+	{
+	    Point other = (Point)obj;
+	    if (other.x == x) {
+		return y - other.y;
+	    }	
+	    return x - other.x;
+	}
+
+
+	/// <summary>
+	///   文字列化メソッド
+	/// </summary>
+	public override string ToString()
+	{
+	    return "(" + x + ", " + y + ")";
+	}
+	
+    } // Point
+  
+}
