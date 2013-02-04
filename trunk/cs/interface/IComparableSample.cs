5434189089e1a80d8299de4647c388fd5cd7c05c
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-04 14:27:11 +0000

    Generics版 Point のサンプルを IComparable<T> で作成

--- trunk/cs/interface/IComparableSample.cs
+++ trunk/cs/interface/IComparableSample.cs
@@ -14,31 +14,18 @@ namespace IComparableSample
     
     class Program
     {
-	static void WriteTitle(string title)
-	{
-	    Console.WriteLine();
-	    Console.WriteLine(title);
-	    Console.WriteLine(new String('=', title.Length));
-	    Console.WriteLine();
-	}
-	
 	static void DumpArray<Type>(string title, IEnumerable<Type> ary)
 	{	    
-	    Console.WriteLine("## " + title + " ##\n");
-	    Console.Write("    ");
+	    Console.WriteLine("## " + title + " ##");
 	    foreach(var it in ary)
 	    {
 	    	Console.Write("{0} ", it);
 	    }
-	    Console.WriteLine();
-	    Console.WriteLine();
+	    Console.WriteLine("\n");
 	}
 	
         static void Main()
         {
-	    // 配列のソートと検索
-	    WriteTitle("Array");
-	    
 	    // Point を格納したデータを準備
 	    Point [] ary = {
 		new Point(1, 2),
@@ -54,31 +41,10 @@ namespace IComparableSample
 	    DumpArray("Sorted", ary);
 
 	    // 二分探索
-	    Console.WriteLine("## Binary Search ##\n  ");
+	    Console.WriteLine("## Binary Search ##");
 	    var target = new Point(3, 1);
 	    int pos = Array.BinarySearch(ary, target);
-	    Console.WriteLine("    {0} => {1}", target, pos);
-
-
-	    Console.WriteLine("\n");
-	    // SortedList (Key, Value のペアーを Key でソートして格納するコンテナー)
-	    WriteTitle("SortedList");
-
-	    // Point を格納したデータを準備
-	    SortedList<Point, string> alist = new SortedList<Point, string>();
-	    Point p = null;
-	    p = new Point(1, 2); alist.Add(p, "A"); Console.WriteLine("    Add {0}, A", p);
-	    p = new Point(3, 5); alist.Add(p, "B"); Console.WriteLine("    Add {0}, B", p);
-	    p = new Point(1, 9); alist.Add(p, "C"); Console.WriteLine("    Add {0}, C", p);
-	    p = new Point(4, 1); alist.Add(p, "D"); Console.WriteLine("    Add {0}, D", p);
-	    p = new Point(3, 1); alist.Add(p, "E"); Console.WriteLine("    Add {0}, E", p);
-
-	    Console.WriteLine("\n## SortedList Contents ##\n");
-	    foreach( KeyValuePair<Point, string> kvp in alist )
-	    {
-	    	Console.WriteLine("    {0} : {1}", kvp.Key, kvp.Value);
-	    }
-
+	    Console.WriteLine("{0} @ {1}", target, pos);
         }
     }
 }
