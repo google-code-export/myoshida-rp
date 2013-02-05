f31e9c8b6ea848d31fea391bc662f2ceb21815a6
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-05 16:48:53 +0000

    yield ブロックのサンプルを追加

---
+++ trunk/cs/interface/YieldBlockSample.cs
@@ -0,0 +1,82 @@
+//
+// yield ブロックのサンプル
+//
+// コンパイル : csc YieldBlockSample.cs Point.cs
+
+using System;
+using System.Collections;
+
+using Geometry;
+
+
+namespace IEnumerableSample
+{
+    class Polygon : IEnumerable
+    {
+	private Point[] _points;
+
+	public Polygon(Point[] points)
+	{
+	    if (points != null)
+		_points = (Point[])points.Clone();
+	}
+
+	public int Count
+	{
+	    get { return _points.Length; }
+	}
+
+	/// <summary>
+	/// 列挙子を取得
+	/// </summary>
+	public IEnumerator GetEnumerator()
+	{
+	    foreach (Point pos in _points)
+	    {
+		yield return pos;
+	    }
+	}
+
+	/// <summary>
+	///   閉じた折れ線の取得
+	/// </summary>
+	public IEnumerable ClosedPolyline()
+	{
+	    foreach (Point pos in _points)
+	    {
+		yield return pos;
+	    }
+	    yield return _points[0];
+	}
+	
+    }
+
+    class Program
+    {
+	static void Main()
+	{
+	    Point[] points = new Point[] {
+		new Point(0, 0),
+		new Point(5, 0),
+		new Point(0, 5)
+	    };
+	    Polygon poly = new Polygon(points);
+	    
+	    Console.Write("Polygon = {\n  ");
+	    foreach (Point pos in poly)
+	    {
+		Console.Write("{0} ", pos);
+	    }
+	    Console.WriteLine("\n}");
+
+	    
+	    Console.Write("Closed Polyline = {\n  ");
+	    foreach (Point pos in poly.ClosedPolyline())
+	    {
+		Console.Write("{0} ", pos);
+	    }
+	    Console.WriteLine("\n}");
+	}
+    }
+
+}
