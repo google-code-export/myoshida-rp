58f155bbc08bb9ef0b50590a2015b79319f44cc6
yohshiy@gmail.com <yohshiy@gmail.com@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-06 04:27:44 +0000

    IEnumerable のサンプル追加

---
+++ trunk/cs/interface/ICloneableSample.cs
@@ -0,0 +1,37 @@
+﻿//
+// ICloneable のサンプル
+//
+// コンパイル : csc ICloneableSample.cs Point.cs
+
+using System;
+
+using Geometry;
+
+
+namespace ICloneableSample
+{
+
+    class Program
+    {
+	static void Main(string[] args)
+	{
+	    Point a = new Point(1, 2);
+	    Point b = a;
+	    Point c = (Point)a.Clone();
+
+	    Console.WriteLine("==== Base ====");
+	    Console.WriteLine("a = {0}", a);
+	    Console.WriteLine("b = {0}", b);
+	    Console.WriteLine("c = {0}", c);
+	    Console.WriteLine();
+
+	    a.x = 5;
+	    Console.WriteLine("==== Changed A (a.x = 5) ====");
+	    Console.WriteLine("a = {0}", a);
+	    Console.WriteLine("b = {0}", b);      
+	    Console.WriteLine("c = {0}", c);      
+	}
+    }  
+}
+
+
