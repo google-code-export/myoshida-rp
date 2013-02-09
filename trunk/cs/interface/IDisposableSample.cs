62826b10af7b4ea6bb452089514763fd63454553
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-09 11:08:08 +0000

    IEnumerable<T> と IDisposable のサンプル追加

---
+++ trunk/cs/interface/IDisposableSample.cs
@@ -0,0 +1,98 @@
+﻿//
+// IDisposable のサンプル
+//
+// コンパイル : csc IDisposableSample.cs
+
+using System;
+using System.Web;
+using System.IO;
+using System.Text;
+
+namespace IDisposableSample
+{
+    class HtmlFileWriter : IDisposable
+    {
+	StreamWriter m_writer;
+
+	private void WriteHeader()
+	{
+	    m_writer.WriteLine("<html>");
+	    m_writer.WriteLine("<head>");
+	    m_writer.WriteLine("<title> Sample </title>");
+	    m_writer.WriteLine("</head>");
+	    m_writer.WriteLine("<body>");
+	}	
+
+	private void WriteFooter()
+	{
+	    m_writer.WriteLine("</body>");
+	    m_writer.WriteLine("</html>");
+	}	
+
+	
+	#region 初期化
+
+	public HtmlFileWriter(string fpath)
+	{
+	    m_writer = File.CreateText(fpath);
+	    WriteHeader();
+	}	
+
+	#endregion
+
+	public void Close()
+	{
+	    if (m_writer != null)
+	    {
+		WriteFooter();
+		m_writer.Close();
+		m_writer = null;
+	    }	
+	}	
+
+	public void Dispose()
+	{
+	    Close();
+	}
+	
+	public void Write(string str)
+	{
+	    m_writer.Write(HttpUtility.HtmlEncode(str));
+	}	
+
+	public void WriteRaw(string str)
+	{
+	    m_writer.Write(str);
+	}	
+	
+	public void WriteLine(string str = "")
+	{
+	    Write(str);
+	    m_writer.WriteLine(" <br />");    
+	}	
+	
+	public static HtmlFileWriter Create(string str)
+	{
+	    return new HtmlFileWriter(str);
+	}
+	
+    } // Point
+
+    class Program
+    {
+	static void Main(string[] args)
+	{
+	    string samplefpath = "sample.html";
+
+	    using (HtmlFileWriter fp = HtmlFileWriter.Create(samplefpath))
+	    {
+		fp.WriteLine("test");
+		fp.WriteLine("<test>");
+		fp.WriteRaw("<strong>Test</strong>");
+		fp.WriteLine();
+	    }
+	}
+    }  
+}
+
+
