1b91cfeeca8977f3cacb11e85564cf4161c40333
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-02-18 17:30:46 +0000

    Dispose() の処理を修正

--- cs/interface/IDisposableSample.cs
+++ cs/interface/IDisposableSample.cs
@@ -12,69 +12,112 @@ namespace IDisposableSample
 {
     class HtmlFileWriter : IDisposable
     {
-	StreamWriter m_writer;
+	// 後処理の管理が必要なメンバー
+	StreamWriter _writer;
+	// 通常のメンバー
+	string _title;
+	// Dispose 
+        private bool disposed = false;
 
 	private void WriteHeader()
 	{
-	    m_writer.WriteLine("<html>");
-	    m_writer.WriteLine("<head>");
-	    m_writer.WriteLine("<title> Sample </title>");
-	    m_writer.WriteLine("</head>");
-	    m_writer.WriteLine("<body>");
+	    _writer.WriteLine("<html>");
+	    _writer.WriteLine("<head>");
+	    _writer.WriteLine("<title> " + _title + " </title>");
+	    _writer.WriteLine("</head>");
+	    _writer.WriteLine("<body>");
 	}	
 
 	private void WriteFooter()
 	{
-	    m_writer.WriteLine("</body>");
-	    m_writer.WriteLine("</html>");
+	    _writer.WriteLine("</body>");
+	    _writer.WriteLine("</html>");
 	}	
 
-	
-	#region 初期化
-
-	public HtmlFileWriter(string fpath)
+	public HtmlFileWriter(string fpath, string title = "Sample")
 	{
-	    m_writer = File.CreateText(fpath);
+	    _title = title;
+	    _writer = File.CreateText(fpath);
 	    WriteHeader();
 	}	
 
-	#endregion
 
+	// ファイルのクローズ
 	public void Close()
 	{
-	    if (m_writer != null)
-	    {
-		WriteFooter();
-		m_writer.Close();
-		m_writer = null;
-	    }	
+	    // ファイルストリームを真似て、
+	    // Close() と Dispose() を同じ処理にします。
+	    Dispose();
 	}	
 
-	public void Dispose()
-	{
-	    Close();
-	}
 	
 	public void Write(string str)
 	{
-	    m_writer.Write(HttpUtility.HtmlEncode(str));
+	    _writer.Write(HttpUtility.HtmlEncode(str));
 	}	
 
 	public void WriteRaw(string str)
 	{
-	    m_writer.Write(str);
+	    _writer.Write(str);
 	}	
 	
 	public void WriteLine(string str = "")
 	{
 	    Write(str);
-	    m_writer.WriteLine(" <br />");    
+	    _writer.WriteLine(" <br />");    
 	}	
 	
 	public static HtmlFileWriter Create(string str)
 	{
 	    return new HtmlFileWriter(str);
 	}
+
+
+	// IDisposable に必須のメソッドの実装
+        public void Dispose()
+        {
+            Dispose(true);
+            // Dispose() によってリソースの解放を行ったので、
+            // GC での解放が必要が無いことを GC に通知します。
+            GC.SuppressFinalize(this);
+        }
+
+	// ファイナライザー(デストラクター)
+        //
+        // Dispose
+        ~HtmlFileWriter()
+        {
+            Dispose(false);
+        }
+
+	
+        // このメソッドの呼び出され方は 2 パターンあります。
+        // 
+        // disposing が true であれば、 Dispose() から呼び出されています。
+	//
+        // disposing が false であれば、 ファイナライザー(~HtmlFileWriter)
+        // から呼び出されています。
+        protected virtual void Dispose(bool disposing)
+        {
+            // Dispose がまだ実行されていないときだけ実行
+            if(!this.disposed)
+            {
+                // disposing が true の場合(Dispose() が実行された場合)は
+                // アンマネージリソースも解放します。
+                if(disposing)
+                {
+                    // アンマネージリソースの解放
+                    _title = null;
+                }
+
+		// マネージリソースの解放
+		WriteFooter(); // 閉じる前にフッターを書く
+		_writer.Dispose();
+
+                disposed = true;
+            }
+        }
+
 	
     } // Point
 
