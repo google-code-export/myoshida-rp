87e164fbbde5f6ab003f7d2f9bdb3f1f21536ca5
yohshiy@gmail.com <yohshiy@gmail.com@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-06-06 04:36:16 +0000

    カルチャー変更機能追加

--- trunk/cs/i18n/WpfI18n/App.xaml.cs
+++ trunk/cs/i18n/WpfI18n/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Globalization;	// 追加
+using System.Threading;		// 追加
 
 namespace WpfI18n
 {
@@ -13,5 +15,25 @@ namespace WpfI18n
     /// </summary>
     public partial class App : Application
     {
+        private void SetCurrentUICulture(string culname)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culname);
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {        
+            // 環境変数を使用
+            string langstr = System.Environment.GetEnvironmentVariable("TEMP_LANG");
+            if (langstr != null)
+            {
+                SetCurrentUICulture(langstr);
+            }
+
+            // 引数を使用
+            if (0 < e.Args.Length)
+            {
+                SetCurrentUICulture(e.Args[0]);
+            }
+        }
     }
 }
