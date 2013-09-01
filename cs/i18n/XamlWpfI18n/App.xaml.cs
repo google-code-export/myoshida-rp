5c07e5f0f96616ba5b28fe7472e491ab26b1c850
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-09-01 19:46:37 +0000

    xaml sample adds

---
+++ cs/i18n/XamlWpfI18n/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Threading;
+using System.Globalization;
+
+namespace XamlWpfI18n
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void SetCurrentUICulture(string culname)
+        {
+            CultureInfo ci = new CultureInfo(culname);
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 環境変数を使用
+            string langstr = System.Environment.GetEnvironmentVariable("TEMP_LANG");
+            if (langstr != null)
+            {
+                SetCurrentUICulture(langstr);
+            }
+
+            // 引数を使用
+            if (0 < e.Args.Length)
+            {
+                SetCurrentUICulture(e.Args[0]);
+            }
+        }
+
+    }
+}
---
+++ cs/i18n/XamlWpfI18n/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace XamlWpfI18n
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGreet_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = (string)Application.Current.FindResource("MessageHello");
+            MessageBox.Show(msg);
+
+        }
+    }
+}
