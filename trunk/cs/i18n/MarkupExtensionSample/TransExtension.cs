253886ea8a0b500562b20a0a0a77b1202fc1bd56
yohshiy@gmail.com <yohshiy@gmail.com@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-09-11 06:26:43 +0000

    Adds sample for MarkupExtension.

---
+++ trunk/cs/i18n/MarkupExtensionSample/TransExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Markup;
+
+
+namespace MarkupExtensionSample
+{
+    [MarkupExtensionReturnTypeAttribute(typeof(string))]
+    public class TransExtension : MarkupExtension
+    {
+
+        string _key;
+
+        public TransExtension(string key)
+        {
+            _key = key;
+        }
+
+        const string NotFoundError = "#StringNotFound#";
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(_key))
+                return NotFoundError;
+
+            return Properties.Resources.ResourceManager.GetString(_key) ?? NotFoundError;
+        }
+
+    }
+}
