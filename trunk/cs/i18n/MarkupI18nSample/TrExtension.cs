4cb97ca5518ea27cccaaa9a25562ecd3a93730d8
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-09-16 17:44:14 +0000

    Adds Markup Extension I18n sample.

---
+++ trunk/cs/i18n/MarkupI18nSample/TrExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+
+namespace MarkupI18nSample
+{
+    [MarkupExtensionReturnTypeAttribute(typeof(string))]
+    public class TrExtension : MarkupExtension
+    {
+
+        string _key;
+
+        public TrExtension(string key)
+        {
+            _key = key;
+        }
+
+        const string NotFoundError = "#StringNotFound#";
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(_key))
+                return NotFoundError;
+
+            return Properties.Resources.ResourceManager.GetString(_key) ?? NotFoundError;
+        }
+
+    }
+
+
+}
