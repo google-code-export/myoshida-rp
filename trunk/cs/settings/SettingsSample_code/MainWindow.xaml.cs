78f23c6de297072865d989b383afbdbf37733f32
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2013-11-11 15:30:34 +0000

    Settings sample using code.

---
+++ trunk/cs/settings/SettingsSample_code/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SettingsSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Left   = Properties.Settings.Default.MainWindow_Left;
+            Top    = Properties.Settings.Default.MainWindow_Top;
+            Width  = Properties.Settings.Default.MainWindow_Width;
+            Height = Properties.Settings.Default.MainWindow_Height;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.MainWindow_Left   = Left;
+            Properties.Settings.Default.MainWindow_Top    = Top;
+            Properties.Settings.Default.MainWindow_Width  = Width;
+            Properties.Settings.Default.MainWindow_Height = Height;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
