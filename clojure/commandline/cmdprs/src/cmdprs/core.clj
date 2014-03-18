5a01474a793f1e3986c0cd933f0c480a8d3441e5
yohshiy@gmail.com <yohshiy@gmail.com@31b6770d-4fb5-51c9-0955-c716b99d406c>
2014-03-18 06:33:20 +0000

    Add Used Command line parse sample.

--- clojure/commandline/cmdprs/src/cmdprs/core.clj
+++ clojure/commandline/cmdprs/src/cmdprs/core.clj
@@ -6,34 +6,32 @@
 
 ;; オプション仕様定義
 (def option-spec
-  ;; ショート形式 ロング形式 [オプション説明] [追加設定(キーと値) ...]
   [["-h" "--help" "Show help."]
-   ;; デフォルト値を設定
-   ["-v" "--version" "Show program version." :default false]
-   ;; ショート形式を省略するときは nil
+   ["-v" "--version" "Show program version."]
    [nil "--verbose" "Output log verbosity."]
-
-   ;; 引数を取るオプション
-   ["-o" "--output FILE" "Output file path"
+   ;; 引数付き
+   ["-o" "--output FILE" "Output file path (Default: \"a.out\")"
     :default "a.out"
     ]
    ;; assoc-fn 指定
    ["-e" "--expr PATTERN" "Regular expression pattern"
     :assoc-fn
     (fn [m k v]
-      ;(println (str "m=" m ", k=" k ", v=" v))
+      ; (println (str "m=" m ", k=" k ", v=" v))
+      ; (assoc m k v)
       (assoc m k
              (let [vec (get m k)]
                (if vec (conj vec v) [v])))
       )
     ]
-   ;; A boolean option defaulting to nil
    ])
 
 
 (defn -main
   "Command line option parse sample program."
   [& args]
-  (pprint
-   ;; (parse-opts 引数リスト オプション仕様
-   (parse-opts args option-spec)))
+  ;; 引数を解析 (parse-opts 引数リスト オプション仕様)
+  (let [info (parse-opts args option-spec)]
+    ;; 解析結果を使用
+    (pprint info))
+  )
