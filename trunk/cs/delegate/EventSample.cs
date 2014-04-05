5aeb7c4ce7d2f98a66059929cf4b82cfa5c67adf
yoshida@sheep.dog.cx <yoshida@sheep.dog.cx@31b6770d-4fb5-51c9-0955-c716b99d406c>
2014-04-05 19:02:20 +0000

    delegate と event のサンプルを追加

---
+++ trunk/cs/delegate/EventSample.cs
@@ -0,0 +1,62 @@
+﻿//
+// event のサンプル
+//
+// コンパイル : csc SimpleEventSample.cs
+
+using System;
+
+
+namespace EventSample
+{
+    delegate void HelloDelegate(string name);
+    
+    class Button
+    {
+	// イベント
+	// (クラス定義内でしか event は定義できない)
+	public event HelloDelegate SomeEvent;
+
+	// イベントを発生させる
+	public void Clicked(string name)
+	{
+	    // イベントの発生
+	    // (直接には Foo クラス内でしか呼べない)
+	    SomeEvent(name);
+	}
+    }
+
+    class Foo
+    {
+    	public void Hello(string name)
+    	{
+    	    Console.WriteLine("(method) : Hello {0}!", name);
+    	}
+    }
+
+    class Program
+    {
+    	static void StaticHello(string name)
+    	{
+    	    Console.WriteLine("(static) : Hello {0}!", name);
+    	}
+
+	static void Main(string[] args)
+	{
+	    Foo foo = new Foo();
+
+	    Button btn = new Button();
+
+	    // 関数の登録(アタッチ)
+	    btn.SomeEvent += foo.Hello;
+	    btn.SomeEvent += StaticHello;
+	    btn.SomeEvent += (string name) => {
+		Console.WriteLine("(lambda) : Hello {0}!", name);
+	    };
+
+	    // イベントの発生
+	    btn.Clicked("world");
+	}
+    }
+}
+
+
